c379531728918d312ca51bfb1d665f3d9e46b24d
spureful <rw1zz@yzandex.ru>
2018-06-07 20:12:43 +0300

    add header

--- src/assets/styles/variables.js
+++ src/assets/styles/variables.js
@@ -2,7 +2,7 @@ module.exports = {
   white: "#efeee9",
   beige: "#eeebdf",
   green: "#16c2aa",
-  darkgreen: "#16c2aa",
+  darkgreen: "#009688",
   "text-color": "#455a64",
   grey: "#373e42",
   "light-grey": "#dfdcd5"
