93965723872379352d63c2c248426e9400dbe829
spureful <rw1zz@yzandex.ru>
2018-06-23 12:36:15 +0300

    almost made skills

--- src/assets/scripts/modules/menu.js
+++ src/assets/scripts/modules/menu.js
@@ -4,84 +4,34 @@ function menuAppear() {
   const menuBlock = document.querySelector(".js-menu");
   const leftDoor = document.querySelector(".hamburgermenu__leftdoor");
   const rightDoor = document.querySelector(".hamburgermenu__rightdoor");
-  let doorcountLeft = -100;
-  let doorcountRight = 100;
-  let doorcountLeftAfter = 0;
-  let doorcountRightAfter = 0;
-  let opD = 1;
-  let opM = 0;
-  let closeInit = false;
-  let openInit = false;
+  const menuItems = document.querySelectorAll(".js-menu-items");
+  const elStyle = getComputedStyle(leftDoor);
+  const wPos = elStyle.width;
 
   openBtn.addEventListener("click", function() {
-   	leftDoor.classList.add("hamburgermenu__leftdoor_active");
-	rightDoor.classList.add("hamburgermenu__rightdoor_active");
-	menuBlock.classList.add("js-menu_visible");
-	doorcountLeft = -100;
-    doorcountRight = 100;
-    opD = 1;
-    opM = 0; 	
-  
-     setTimeout(function doors() {
-       if (doorcountLeft <= 0 && doorcountRight >= 0) {
-          leftDoor.style.transform = "translateX(" + doorcountLeft + "%" + ")";
-          rightDoor.style.transform = "translateX(" + doorcountRight + "%" + ")";
-          doorcountLeft += 2;
-          doorcountRight -= 2;
-		
-        }
-        setTimeout(doors, 40);
-		
-      openInit = true;
-         
-      }, 40);
-   
-     if(openInit = true) {
-      setTimeout(function foo() {
-        if (opD > 0 && opM <1) {
-          leftDoor.style.opacity = opD;
-          rightDoor.style.opacity = opD; 
-          menuBlock.style.opacity = opM; 
-          opD -= 0.1;
-          opM += 0.1;
-		  
-	   }
-	   setTimeout(foo, 50);
-	 }, 2500);
-     }
-  });
-  
-  
     
-  closeBtn.addEventListener("click", function() {
-    doorcountLeftAfter = 0;
-    doorcountRightAfter = 0;
-    setTimeout(function closeDoor() {
-     if (doorcountLeftAfter >= -100 && doorcountRightAfter <= 100) {
-      leftDoor.style.opacity = 1; 
-      rightDoor.style.opacity = 1;
-       menuBlock.style.opacity = 0;
-      leftDoor.style.transform = "translateX(" + doorcountLeftAfter + "%" + ")";
-      rightDoor.style.transform = "translateX(" + doorcountRightAfter + "%" + ")";
-      doorcountLeftAfter -= 2;
-      doorcountRightAfter += 2;
-   
-     }
-      setTimeout(closeDoor, 50);
-       
-      closeInit = true;
-         
-    }, 50);
-      
-    if (closeInit = true) {  
-     setTimeout (function closeFoo() { leftDoor.classList.remove("hamburgermenu__leftdoor_active");
-      rightDoor.classList.remove("hamburgermenu__rightdoor_active");
-	  menuBlock.classList.remove("js-menu_visible");
- }, 2500);
-    }
+    leftDoor.style.width = 50 + "%";
+    rightDoor.style.width = 50 + "%";
 	
-
+	setTimeout(function foo() {
+	
+    menuBlock.classList.add("js-menu_visible");
+    menuBlock.style.transitionDuration = 0.5 + "s";
+	}, 1300);
+ 
+	  });
+  closeBtn.addEventListener("click", function() {
+	  
+    menuBlock.classList.remove("js-menu_visible");
+    menuBlock.style.transitionDuration = 0.5 + "s";
+    
+	setTimeout(function foo() {
+	  leftDoor.style.width = 0 + "%";
+      rightDoor.style.width = 0 + "%";
+     
+	}, 100);
   });
 }
+
 
 menuAppear();
--- src/assets/scripts/modules/skills.js
+++ src/assets/scripts/modules/skills.js
@@ -1,31 +1,111 @@
-//const skills = [
-//  {
-//    titleGruop: "Frontend",
-//    titleSkills: ["Html5", "css3", "avaScript & jQuery"],
-//    skillPercents: ["80", "90", "60"]
-//  },
-//  {
-//    titleGruop: "Backend",
-//    titleSkills: ["PHP", "css3", "mySQL", "Node.js& npm"],
-//    skillPercents: ["30", "10", "30", "40"]
-//  },
-//  {
-//    titleGruop: "Workflow",
-//    titleSkills: ["Git", "Gulp", "Bower"],
-//    skillPercents: ["80", "90", "60"]
-//  }
-//];
-//
-//const skillGroupTitle = document.querySelectorAll(".js-sckill-grouptitle");
-//const skillTitle = document.querySelectorAll(".js-skill-title");
-//const skillPersent = document.querySelectorAll(".js-skill-persents");
-//const skillItem = document.querySelectorAll(".js-skill-items");
-//
-//function skillsPercents() {
-//  for (let i = 0; i < skills.length; i++) {
-//    skillGroupTitle[i].innerText = skills[i].titleGruop;
-//
-//  }
-//}
-//
-//skillsPercents();
+import Vue from "vue";
+
+const skills = [
+  {
+    "titleGruop": "Frontend",
+	"skillsCont": {
+      "Html5": 90,
+      "css3": 85,
+      "javaScript & jQuery": 50
+		 
+	 }
+
+  },
+  {
+    titleGruop: "Backend",
+	"skillsCont": {
+      "PHP": 10,
+      "mySQL": 30,
+      "Node.js": 40,
+      "Mongo.db": 50 
+		 
+	 }
+   
+  },
+  {
+    titleGruop: "Workflow",
+	"skillsCont": {
+		"Git": 50,
+		 "Gulp": 30,
+		 "Bower": 20
+		 
+	 }
+  
+  }
+];
+
+
+
+const skill = {
+  template: "#skills-row-item",
+  props: {
+	titleSkills: String,
+    skillPercents: Number
+  },
+  methods: {
+    drawCircle() {
+	  const circle = this.$refs["color-circle"];
+	  const dash = parseInt(
+        getComputedStyle(circle).getPropertyValue("stroke-dashoffset")
+	  );
+      const persents =(dash/100) * (100 - this.skillPercents);
+	 
+      circle.style.strokeDashoffset = persents;
+		
+	  	}
+  },
+  mounted() {
+	
+    this.drawCircle();
+  }  
+};
+const skillsRow = {
+  template: "#skills-row",
+  components: {
+    skill
+  },
+  props: {
+    skill: Object
+  }
+};
+
+new Vue({
+  el: "#aboutme__skills-block",
+  components: {
+    skillsRow
+  },
+  data: {
+    skillscontent: {}
+  },
+  created() {
+    const dataSkills = skills;
+    this.skillscontent = dataSkills;
+   
+  },
+	
+  template: "#skills-section", 
+  methods: {
+ findblock() {	 
+     const skillBlock = this.$refs["skills-block"];
+  
+    window.onscroll = function() {
+    const posTop = skillBlock.getBoundingClientRect().top
+	const fff = skillBlock.offsetTop;
+    const exactTop = posTop.toFixed();
+    console.log(exactTop);
+	if (exactTop > 200 && exactTop < 210) {
+	  console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaa");
+//		skill.methods.drawCircle();
+		}
+}
+	  
+
+  }
+  },
+	
+  mounted() {
+	  this.findblock();
+  }
+ 
+
+  });
